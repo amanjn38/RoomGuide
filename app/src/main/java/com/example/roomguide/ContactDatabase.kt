71ffe667cb65480aa6cae31c13df845d2b68cd6f
amanjn38 <amanjn38@gmail.com>
2023-05-12 02:46:37 +0530

    Initial commit.

---
+++ app/src/main/java/com/example/roomguide/ContactDatabase.kt
@@ -0,0 +1,13 @@
+package com.example.roomguide
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Contact::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class ContactDatabase : RoomDatabase(){
+    abstract val dao: ContactDao
+}
---
+++ app/src/main/java/com/example/roomguide/SortType.kt
@@ -0,0 +1,7 @@
+package com.example.roomguide
+
+enum class SortType {
+    FIRST_NAME,
+    LAST_NAME,
+    PHONE_NUMBER
+}
